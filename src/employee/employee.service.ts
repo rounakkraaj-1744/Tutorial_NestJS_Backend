4dc110407e16845ebd365db12ad17f51173ac2ce
rounakkraaj-1744 <rounakkraaj707@gmail.com>
2025-01-25 13:33:22 +0530

    added more return info

--- src/employee/employee.service.ts
+++ src/employee/employee.service.ts
@@ -2,11 +2,16 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class EmployeeService {
+  obj = [{
+    "name": "abc",
+    "emp_id": 123,
+    "email": "abc@gmail.com"
+  },{
+    "name": "xyz",
+    "emp_id": 456,
+    "email": "xyz@gmail.com"
+  }]
   getEmployeeInfo(){
-    return {
-      "name":"ABC",
-      "id":123,
-      "email":"abc@gmail.com"
-    }
+    return this.obj;
   }
 }
