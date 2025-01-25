2d5a84276a4a7103154e86f6a25ba17fa355ac7e
rounakkraaj-1744 <rounakkraaj707@gmail.com>
2025-01-25 14:34:34 +0530

    updated the employee.module.ts

--- src/employee/employee.module.ts
+++ src/employee/employee.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EmployeeService } from './employee.service';
 import { EmployeeController } from './employee.controller';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Module({
   controllers: [EmployeeController],
-  providers: [EmployeeService],
+  providers: [EmployeeService, PrismaService],
 })
 export class EmployeeModule {}
