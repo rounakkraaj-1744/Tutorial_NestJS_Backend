0322d70a0750212db8f7d6d2c7a77b61595baae5
rounakkraaj-1744 <rounakkraaj707@gmail.com>
2025-01-30 20:16:10 +0530

    added custom pipe for parseIntPipe

--- src/pipe/integer.pipe.ts
+++ src/pipe/integer.pipe.ts
@@ -3,6 +3,6 @@ import { Injectable, PipeTransform } from "@nestjs/common";
 @Injectable()
 export class IntegerPipe implements PipeTransform{
     transform(value: any) {
-        return Number(value)
+        return Number(value);
     }
 }
